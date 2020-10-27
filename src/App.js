5db1326ef881051c877729071807b7517697a676
phlgr <philippm.gartz@gmail.com>
2020-10-27 15:31:03 +0100

    Use ImagePreview component and style main element

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./global.css";
+import "./App.css";
+import ImagePreview from "./components/ImagePreview";
 
 function App() {
-  return <></>;
+  return (
+    <main>
+      <button>Get Random Image</button>
+      <ImagePreview
+        src={
+          "https://images.unsplash.com/photo-1603614486387-276f74fcbe2a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1947&q=80"
+        }
+        alt={"Newspaper reading"}
+        author={"Flip"}
+      />
+    </main>
+  );
 }
 
 export default App;
