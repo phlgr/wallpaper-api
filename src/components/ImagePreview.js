7cc9edeb796c998f34d6f0f38247ec1bd5faf118
lmachens <leon.machens@googlemail.com>
2020-10-28 12:08:32 +0100

    Add icon button

--- src/components/ImagePreview.js
+++ src/components/ImagePreview.js
@@ -1,9 +1,11 @@
 import React from "react";
+import IconButton from "./IconButton";
 import "./ImagePreview.css";
 
 export default function ImagePreview({ src, alt, author }) {
   return (
     <div className="imageContainer">
+      <IconButton>💖</IconButton>
       <img className="imageThumb" src={src} alt={alt} />
       <p className="imageAuthor">Author: {author}</p>
     </div>
