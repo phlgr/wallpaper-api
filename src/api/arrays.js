bd6d297650c33764239f443f6eb03c62d8d0ed82
lmachens <leon.machens@googlemail.com>
2020-10-29 15:34:19 +0100

    Add array helper

---
+++ src/api/arrays.js
@@ -0,0 +1,4 @@
+export function without(array, value) {
+  // Add logic and keep smiling 🤗
+  return null;
+}
---
+++ src/api/arrays.test.js
@@ -0,0 +1,13 @@
+const { without } = require("./arrays");
+
+it("creates new numbers array without a number", () => {
+  const numbers = [1, 3, 7, 8, 11, 13];
+  const primNumbers = without(numbers, 8);
+  expect(primNumbers).toEqual([1, 3, 7, 11, 13]);
+});
+
+it("creates new string array without a string", () => {
+  const photoIds = ["UjD08DRW24E", "k38v31SkZdo", "AW_P_NU9MI8"];
+  const newPhotoIds = without(photoIds, "UjD08DRW24E");
+  expect(newPhotoIds).toEqual(["k38v31SkZdo", "AW_P_NU9MI8"]);
+});
